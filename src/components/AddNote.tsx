78cc7ea16832d1e31273b1b0dd99020605bab411
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2025-05-03 15:41:08 +0530

    Update button styles in AddNote and NoteItem components for improved UI consistency and visibility

--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -15,7 +15,7 @@ export function AddNote() {
     <div className="relative">
       <div className="flex bg-slate-700 p-3 items-center w-full gap-2 rounded-t-xl">
         <input className="p-1 px-2 shadow-inner bg-slate-800 text-slate-300 rounded-md w-full" type="text" value={newNote} ref={inputRef} onChange={(e) => setNewNote(e.target.value)} />
-        <button className="px-2 py-1 bg-slate-900 text-slate-400 rounded-md" onClick={handleClick}>Add</button>
+        <button className="px-2 py-1 bg-slate-900 text-blue-600 rounded-md" onClick={handleClick}>Add</button>
       </div>
     </div>
   )
--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -3,11 +3,11 @@ import { useNotesStore } from "../store/notes-store"
 export function NoteItem({ note }: { note: string }) {
   const removeNote = useNotesStore(state => state.removeNote)
   return (
-    <div className="flex justify-between p-2 mx-2 shadow-md rounded-md bg-slate-800 text-slate-400">
+    <div className="group flex justify-between p-2 mx-2 shadow-md rounded-md bg-slate-800 text-slate-400">
       <div>
         {note}
       </div>
-      <button className="s self-start" onClick={() => removeNote(note)}>Delete</button>
+      <button className="self-start text-red-700 opacity-0 group-hover:opacity-100 transition-all" onClick={() => removeNote(note)}>Delete</button>
     </div>
   )
 }
